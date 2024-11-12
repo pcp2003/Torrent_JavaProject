fdd19b3242fa146974da2c6646230f125f758b30
Guilherme Penedo <guipedroborges@gmail.com>
2024-11-12 10:59:11 +0000

    ponto 2

--- FileBlockRequestMessage.java
+++ FileBlockRequestMessage.java
@@ -1,3 +1,13 @@
-public class FileBlockRequestMessage {
-    
+public class FileBlockRequestMessage{
+    private String fileHash;
+    private long offset;
+    private long length;
+
+    FileBlockRequestMessage(String fileHash, long offset, long length){
+        this.fileHash = fileHash;
+        this.offset = offset;
+        this.length = length;
+    }
+
+
 }
--- Node.java
+++ Node.java
@@ -1,8 +1,10 @@
+import java.io.File;
 import java.time.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Node extends Thread{
+public class Node{
     public class NewConnectionRequest {
         Node sender;
         LocalDateTime time;
@@ -11,6 +13,27 @@ public class Node extends Thread{
             time = t;
         }
     }
+    public class FileSearcher extends Thread {
+        String toFind;
+        FileSearcher(String f){
+            toFind = f;
+        }
+        @Override
+        public void run(){
+            try{
+                for(Node node: knownedNodes){
+                    String[] files = node.getFileList();
+                    for(String file : files){
+                        if(file.equals(toFind)) break;
+                    }
+                }
+
+            }catch(InterruptedException e){
+                throw e;
+            }
+        }
+    }
+
     private List<Node> knownedNodes = new ArrayList<Node>() ;
     Node(List<Node> nodes){
         if(nodes.isEmpty()) throw new IllegalArgumentException("Node list is empty");
@@ -23,10 +46,8 @@ public class Node extends Thread{
     public void connect(NewConnectionRequest req){
         knownedNodes.add(req.sender);
     }
-    @Override
-    public void run () {
-        try {
-            System.out.println("Node: " + knownedNodes.get(knownedNodes.size()-1).toString());
-        } catch ( InterruptedException e ) {}
+
+    private String[] getFileList() {
+        return new File("/files").list();
     }
 }
